6be313e020d0645eeb93bb4680de0624fd650fe8
kriskata <kriskata5000@gmail.com>
2025-01-03 11:20:13 +0200

    Init

---
+++ src/main/java/we/newsapp/newsappserver/NewsappserverApplication.java
@@ -0,0 +1,13 @@
+package we.newsapp.newsappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsappserverApplication.class, args);
+	}
+
+}
