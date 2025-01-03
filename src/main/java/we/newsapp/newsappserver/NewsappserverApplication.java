20071755de0ec8d734ff7b0ef4c168da5f95748a
kriskata <kriskata5000@gmail.com>
2025-01-03 15:58:00 +0200

    Wanna add HTTPS

--- src/main/java/we/newsapp/newsappserver/NewsappserverApplication.java
+++ src/main/java/we/newsapp/newsappserver/NewsappserverApplication.java
@@ -2,12 +2,38 @@ package we.newsapp.newsappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import we.newsapp.newsappserver.database.DatabaseManager;
+import we.newsapp.newsappserver.models.News;
 
-@SpringBootApplication
+import java.util.Collections;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class NewsappserverApplication {
+	public static DatabaseManager databaseManager;
 
 	public static void main(String[] args) {
-		SpringApplication.run(NewsappserverApplication.class, args);
+		// Set password: 1@#c4V5B6N7M8(0,(*mN76B5V4c3347E65R*^T&y^&r%6E4W5C3
+		// Command: INSERT INTO Admins VALUES ('1@#c4V5B6N7M8(0,(*mN76B5V4c3347E65R*^T&y^&r%6E4W5C3');
+		//
+
+
+
+
+		databaseManager = new DatabaseManager();
+		try {
+			databaseManager.setup();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+
+
+		SpringApplication app = new SpringApplication(NewsappserverApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", 3));
+		app.run(args);
 	}
 
 }
+
+
--- src/main/java/we/newsapp/newsappserver/controllers/NewsContoller.java
+++ src/main/java/we/newsapp/newsappserver/controllers/NewsContoller.java
@@ -1,7 +1,23 @@
 package we.newsapp.newsappserver.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import we.newsapp.newsappserver.models.SaneBB;
+import we.newsapp.newsappserver.models.News;
 
 @RestController
 public class NewsContoller {
+
+    @GetMapping("/news")
+    public News getAllNews(@RequestParam(value = "limit", defaultValue = "0") int limit) {
+        return new News();
+    }
+
+    @PostMapping("/news")
+    public void s(String a) {
+        SaneBB.Result b = SaneBB.parse(a);
+        System.out.println(b.html);
+    }
 }
